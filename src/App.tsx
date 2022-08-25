77dd06cdb19af7eb2a6573e6602c5d180a2915ed
fancn21th <fancn21th@gmail.com>
2022-08-25 16:44:32 +0800

    rtk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { amountAdded } from "./features/counter/counterSlice";
-import reactLogo from "./assets/react.svg";
+import { useFetchBreedsQuery } from "./features/dogs/dogsSlice";
+// import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -10,9 +11,17 @@ function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
 
+  const [numberOfDogs, setNumberOfDogs] = useState(10);
+
+  const { data = [], isFetching } = useFetchBreedsQuery(numberOfDogs);
+
   const onClick = () => {
     // dispatch(incremented());
     dispatch(amountAdded(3));
+  };
+
+  const onChange = (e) => {
+    setNumberOfDogs(Number(e.target.value));
   };
 
   return (
@@ -24,7 +33,31 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <select value={numberOfDogs} onChange={onChange}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+        </select>
+        <div>
+          Number of dogs fetched: {data.length}
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.id}</td>
+                  <td>
+                    <img src={breed.image.url} alt="" height={250} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </p>
     </div>
   );
